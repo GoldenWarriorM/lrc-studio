4dfde1c6a4bd634ce1a089d80b1be6c6e6023924
GoldenWarriorM <goldenwarriorm@users.noreply.github.com>
2026-06-03 16:31:19 +0300

    fix: convert px to Dp using density scaling instead of toDp()

--- composeApp/src/androidMain/kotlin/com/lrcstudio/app/util/BottomPadding.android.kt
+++ composeApp/src/androidMain/kotlin/com/lrcstudio/app/util/BottomPadding.android.kt
@@ -6,12 +6,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toDp
 
 @Composable
 actual fun fabBottomPadding(): Dp {
     val density = LocalDensity.current
     val navBarInsetPx = WindowInsets.navigationBars.getBottom(density)
-    val navBarInset = with(density) { navBarInsetPx.toDp() }
+    val navBarInset = (navBarInsetPx / density.density).dp
     return navBarInset + 80.dp
 }
