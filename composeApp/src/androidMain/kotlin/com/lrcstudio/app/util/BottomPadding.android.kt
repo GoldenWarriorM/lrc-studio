292ec7b024cd69ba57792c55e9aaf12f41e93f8b
GoldenWarriorM <goldenwarriorm@users.noreply.github.com>
2026-06-03 16:27:27 +0300

    fix: convert navigation bar inset from px to Dp

--- composeApp/src/androidMain/kotlin/com/lrcstudio/app/util/BottomPadding.android.kt
+++ composeApp/src/androidMain/kotlin/com/lrcstudio/app/util/BottomPadding.android.kt
@@ -6,10 +6,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toDp
 
 @Composable
 actual fun fabBottomPadding(): Dp {
     val density = LocalDensity.current
-    val navBarInset = WindowInsets.navigationBars.getBottom(density)
+    val navBarInsetPx = WindowInsets.navigationBars.getBottom(density)
+    val navBarInset = with(density) { navBarInsetPx.toDp() }
     return navBarInset + 80.dp
 }
